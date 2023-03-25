5983ef0c677236625d3666e332f58fa1355b5140
processos <processos.salsarommanel@gmail.com>
2023-03-25 09:27:59 -0300

    guia.commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let docTitle = document.title 
+
+window.addEventListener('blur', () => {
+    document.title = 'Volte para página de processos'
+})
+
+window.addEventListener('focus', () => {
+    document.title = docTitle
+})
